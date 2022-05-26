8c3f3171df7a96272a5999926bee4caed6a4c03b
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-05-25 23:47:16 -0300

    :pencil: update(mobile): create Dashboard folder and rename files

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Dashboard } from './src/screens/Dashboard';
+import { Dashboard } from './src/screens/Dashboard/';
 
 export default function App() {
   return (
