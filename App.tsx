0a751ec33a6bca5e31d778b48dc81d6289a8fb9b
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-05-26 23:32:24 -0300

    :bento: styles(mobile): add ThemeProvider to app
    - Type theme with Typescript

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
 import { Dashboard } from './src/screens/Dashboard/';
+
+import theme from './src/global/styles/theme';
+
 
 export default function App() {
   return (
-    <Dashboard  />
+    <ThemeProvider theme={theme}>
+      <Dashboard  />
+    </ThemeProvider>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,12 +2,13 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.colors.background };
   align-items: center;
   justify-content: center;
 `
 
 export const Title = styled.Text`
-  color: purple;
+  color: ${({ theme }) => theme.colors.title };
   font-size: 24px;
   font-weight: bold;
 `;
