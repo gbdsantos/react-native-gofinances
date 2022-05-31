3db79a07df9b7dae0f556e8cd87e33db50a55f40
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-05-30 23:54:33 -0300

    :bento: styles(mobile): create header section with some components

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,52 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'; 
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background };
-  align-items: center;
-  justify-content: center;
 `
 
-export const Title = styled.Text`
-  color: ${({ theme }) => theme.colors.title };
+export const UserWrapper = styled.View`
+  padding: 0 24px;
+  width: 100%;
+`;
+
+export const Header = styled.View`
+  background-color: ${({ theme }) => theme.colors.primary };
+  
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+
+  height: ${RFPercentage(42)}px;
+  width: 100%;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+`;
+
+export const Photo = styled.Image`
+  border-radius: 10px;
+
+  height: ${RFValue(48)}px;
+  width: ${RFValue(48)}px;
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape };
   font-family: ${({ theme }) => theme.fonts.regular };
-  font-size: 24px;
+  font-size: ${RFValue(18)}px;
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape };
+  font-family: ${({ theme }) => theme.fonts.bold };
+  font-size: ${RFValue(18)}px;
 `;
