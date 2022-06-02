2cbd4128293f160881b839525996b6de115f0eae
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-06-01 23:55:29 -0300

    :bento: styles(mobile): create HighlightCard component

---
+++ src/components/HighlightCard/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
+
+export const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.shape};
+
+  width: ${RFValue(300)}px;
+`;
+
+export const Header = styled.View``;
+
+export const Title = styled.Text``;
+
+export const Icon = styled(Feather)``;
+
+export const Footer = styled.View``;
+
+export const Amount = styled.Text``;
+
+export const LastTransaction = styled.Text``;
