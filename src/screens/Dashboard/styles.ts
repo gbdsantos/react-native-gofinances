37ca986beb435a7d8a79d1dc661a39d252d70bba
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-06-10 00:13:07 -0300

    :art: style(mobile): create TransactionCard component

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -75,3 +75,15 @@ export const HighlightCards = styled.ScrollView.attrs({
   position: absolute;
   margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions = styled.View`
+  flex: 1;
+  margin-top: ${RFPercentage(12)}px;
+  padding: 0 24px;
+`; 
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular };
+  font-size: ${RFValue(18)}px;
+  margin-bottom: 16px;
+`;
