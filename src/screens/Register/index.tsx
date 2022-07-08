3a5894be7b99a2850194f4d68833415242fc8eaf
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-07-07 21:00:23 -0300

    :sparkles: feat(mobile): integrate react hook form

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Modal } from 'react-native';
+import { useForm } from 'react-hook-form';
 
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
 import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
+import { InputForm } from '../../components/Form/InputForm';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
 
 import { CategorySelect } from '../CategorySelect';
@@ -17,13 +19,23 @@ import {
   TransactionsTypes
 } from './styles';
 
+interface FormData {
+  [name: string]: string;
+}
+
 export function Register(){
   const [category, setCategory] = useState({
     key: 'category',
     name: 'Categoria',
   });
+
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
   const [transactionType, setTransactionType] = useState('');
+
+  const {
+    control,
+    handleSubmit
+  } = useForm();
 
   function handleTransactionsTypesSelect(type: 'up' | 'down') {
     setTransactionType(type);
@@ -37,6 +49,17 @@ export function Register(){
     setCategoryModalOpen(true);
   }
 
+  function handleRegister(form: FormData) {
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key
+    }
+
+    console.log(data);
+  }
+
   return (
     <Container>
       <Header>
@@ -45,10 +68,14 @@ export function Register(){
 
       <Form>
         <Fields>
-          <Input
+          <InputForm
+            control={control}
+            name="name"
             placeholder="Nome"
           />
-          <Input
+          <InputForm
+            control={control}
+            name="amount"
             placeholder="Preço"
           />
 
@@ -74,7 +101,10 @@ export function Register(){
           />
         </Fields>
 
-        <Button title="Enviar" />
+        <Button
+          onPress={handleSubmit(handleRegister)}
+          title="Enviar"
+        />
       </Form>
 
       <Modal visible={categoryModalOpen}>
