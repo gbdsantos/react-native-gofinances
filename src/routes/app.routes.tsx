325f7154ce89c5557d936170f7eed9a782adf89f
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-07-18 13:13:54 -0300

    :art: styles(mobile): customize tab navigation
    - Add icons and customize tab navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const { Navigator, Screen } = createBottomTabNavigator();
@@ -6,22 +7,65 @@ const { Navigator, Screen } = createBottomTabNavigator();
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
 
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
+
 export function AppRoutes() {
+  const theme = useTheme();
+
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 88,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+        }
+      }}
+    >
       <Screen
         component={Dashboard}
         name="Listagem"
+        options={{
+          tabBarIcon: (({size, color}) =>
+            <MaterialIcons
+              color={color}
+              name="format-list-bulleted"
+              size={size}
+            />
+          )
+        }}
       />
 
       <Screen
         component={Register}
         name="Cadastrar"
+        options={{
+          tabBarIcon: (({size, color}) =>
+            <MaterialIcons
+              color={color}
+              name="attach-money"
+              size={size}
+            />
+          )
+        }}
       />
 
       <Screen
         component={Register}
         name="Resumo"
+        options={{
+          tabBarIcon: (({size, color}) =>
+            <MaterialIcons
+              color={color}
+              name="pie-chart"
+              size={size}
+            />
+          )
+        }}
       />
     </Navigator>
   )
